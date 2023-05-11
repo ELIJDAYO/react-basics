cddc32542a7a4674965eada9b5a896313e81d0f9
ELIJDAYO <95513598+ELIJDAYO@users.noreply.github.com>
2023-05-11 16:14:07 +0800

    jsx attributes and styling

--- src/index.js
+++ src/index.js
@@ -1,20 +1,30 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+// import App from './App';
+// import reportWebVitals from './reportWebVitals';
 
-const name = 'Elijah';
-const num = 7;
+const image1 = 'https://picsum.photos/100';
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <div>
-    <h1>Hello World!</h1>
-    <p>
-      Hello {name} {num}
-    </p>
-  </div>,
-  document.getElementById('root')
+    <h1 className="heading">Gallery</h1>
+
+    <img
+      src="/images/1-2-anaconda-picture.png"
+      alt=""
+      className="animal-img"
+    ></img>
+    <img
+      src="/images/1-2-basset-hound-png-clipart.png"
+      alt=""
+      className="animal-img"
+    ></img>
+    <img src="/images/1-2-bat-png.png" alt="" className="animal-img"></img>
+    <img src={image1} alt=""></img>
+    <img src={image1 + '?grayscale'} alt=""></img>
+  </div>
 );
 
 // reportWebVitals();
