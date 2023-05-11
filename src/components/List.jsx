5350d8c6234676e705404335b43937fb900a6f23
ELIJDAYO <95513598+ELIJDAYO@users.noreply.github.com>
2023-05-11 22:02:19 +0800

    implemented components

---
+++ src/components/List.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function List() {
+  return (
+    <ul>
+      <li>Python</li>
+      <li>Javascript</li>
+      <li>Kotlin</li>
+    </ul>
+  );
+}
+
+export default List;
--- src/index.js
+++ src/index.js
@@ -1,30 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-// import App from './App';
-// import reportWebVitals from './reportWebVitals';
-
-const image1 = 'https://picsum.photos/100';
+import Heading from './components/Heading';
+import List from './components/List';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <div>
-    <h1 className="heading">Gallery</h1>
-
-    <img
-      src="/images/1-2-anaconda-picture.png"
-      alt=""
-      className="animal-img"
-    ></img>
-    <img
-      src="/images/1-2-basset-hound-png-clipart.png"
-      alt=""
-      className="animal-img"
-    ></img>
-    <img src="/images/1-2-bat-png.png" alt="" className="animal-img"></img>
-    <img src={image1} alt=""></img>
-    <img src={image1 + '?grayscale'} alt=""></img>
+    <Heading />
+    <List />
   </div>
 );
-
-// reportWebVitals();
