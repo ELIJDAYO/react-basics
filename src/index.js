d5240cf88ba38d4ebb3fa540948a757d2937cf46
ELIJDAYO <95513598+ELIJDAYO@users.noreply.github.com>
2023-05-11 15:48:57 +0800

    js expression

--- src/index.js
+++ src/index.js
@@ -4,10 +4,15 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+const name = 'Elijah';
+const num = 7;
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <div>
     <h1>Hello World!</h1>
+    <p>
+      Hello {name} {num}
+    </p>
   </div>,
   document.getElementById('root')
 );
