2fcbaf84c0eb8ab6658d77f4f043cde96231b240
ELIJDAYO <95513598+ELIJDAYO@users.noreply.github.com>
2023-05-14 17:08:18 +0800

    Enabled react dev tool, implemented components

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import Card from './Card';
 import contacts from '../contacts';
+import Avatar from './Avatar';
 
 function App() {
   return (
     <div>
       <h1 className="heading">My Contacts</h1>
+      <Avatar img="../images/avatar.jpg" />
       {contacts.map((contact) => (
         <Card
           name={contact.name}
