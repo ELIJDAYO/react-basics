51aca19c09aff619a92ef8275c8f69e0e2f826e8
ELIJDAYO <95513598+ELIJDAYO@users.noreply.github.com>
2023-05-15 07:50:03 +0800

    starting file for conditional rendering

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,23 +1,12 @@
 import React from 'react';
-import Card from './Card';
-import contacts from '../contacts';
-import Avatar from './Avatar';
+import Form from './Form';
+
+var userIsRegistered = false;
 
 function App() {
   return (
-    <div>
-      <h1 className="heading">My Contacts</h1>
-      <Avatar img="../images/avatar.jpg" />
-      {contacts.map((contact) => (
-        <Card
-          id={contact.id}
-          key={contact.id}
-          name={contact.name}
-          img={contact.img}
-          tel={contact.tel}
-          email={contact.email}
-        />
-      ))}
+    <div className="container">
+      <Form />
     </div>
   );
 }
