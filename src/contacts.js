45a2138bf26180147c816fa568a785bcce3dc59b
ELIJDAYO <95513598+ELIJDAYO@users.noreply.github.com>
2023-05-14 16:04:30 +0800

    implemented card, props, component, sample data

---
+++ src/contacts.js
@@ -0,0 +1,22 @@
+const contacts = [
+  {
+    name: 'anaconda',
+    img: '../images/1-2-anaconda-picture.png',
+    tel: '+123 456',
+    email: 'anacon@dot.com',
+  },
+  {
+    name: 'hound',
+    img: '../images/1-2-basset-hound-png-clipart.png',
+    tel: '+123 456',
+    email: 'hound@dot.com',
+  },
+  {
+    name: 'bat',
+    img: '../images/1-2-bat-png.png',
+    tel: '+123 456',
+    email: 'bat@dot.com',
+  },
+];
+
+export default contacts;
--- src/index.js
+++ src/index.js
@@ -1,26 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-
-function Card(props) {
-  return (
-    <div>
-      <h2>{props.name}</h2>
-      <img src={props.img} alt="avatar_img" className="animal-img" />
-      <p>{props.tel}</p>
-      <p>{props.email}</p>
-    </div>
-  );
-}
+import App from './components/App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <div>
-    <h1>My contact</h1>
-    <Card
-      name="Anaconda"
-      img="/images/1-2-anaconda-picture.png"
-      tel="+12345"
-      email="ana@con.com"
-    />
+    <App />
   </div>
 );
