87d445b2ef3207c0ef9b913d729d1c213d1f0345
ELIJDAYO <95513598+ELIJDAYO@users.noreply.github.com>
2023-05-15 20:12:02 +0800

    template for even handling

--- src/index.js
+++ src/index.js
@@ -1,37 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import cars from './practice.js';
-
-const [honda, tesla] = cars;
-const {
-  speedStats: { topSpeed: teslaTopSpeed },
-  coloursByPopularity: [teslaTopColour, teslaSecondCol],
-} = tesla;
-const {
-  speedStats: { topSpeed: hondaTopSpeed },
-  coloursByPopularity: [hondaTopColour, hondaSecondCol],
-} = honda;
+import App from './components/App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <table>
-    <tr>
-      <th>Brand</th>
-      <th>Top-Speed</th>
-      <th>Top-Color1</th>
-      <th>Top-Color2</th>
-    </tr>
-    <tr>
-      <td>{tesla.model}</td>
-      <td>{teslaTopSpeed}</td>
-      <td>{teslaTopColour}</td>
-      <td>{teslaSecondCol}</td>
-    </tr>
-    <tr>
-      <td>{honda.model}</td>
-      <td>{hondaTopSpeed}</td>
-      <td>{hondaTopColour}</td>
-      <td>{hondaSecondCol}</td>
-    </tr>
-  </table>
-);
+root.render(<App />);
