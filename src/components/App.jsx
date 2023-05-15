c620968e8457321fdf3ab0c9285c745b2b56550e
ELIJDAYO <95513598+ELIJDAYO@users.noreply.github.com>
2023-05-15 23:52:06 +0800

    template of component tree

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,4 +1,6 @@
 import React, { useState } from 'react';
+import ToDoItem from './ToDoItem';
+import InputArea from './InputArea';
 
 function App() {
   const [inputText, setInputText] = useState('');
@@ -16,21 +18,29 @@ function App() {
     setInputText('');
   }
 
+  function deleteItem(id) {
+    setItems((prevItems) => {
+      return prevItems.filter((item, index) => {
+        return index !== id;
+      });
+    });
+  }
+
   return (
     <div className="container">
       <div className="heading">
         <h1>To-Do List</h1>
       </div>
-      <div className="form">
-        <input onChange={handleChange} type="text" value={inputText} />
-        <button onClick={addItem}>
-          <span>Add</span>
-        </button>
-      </div>
+      <InputArea />
       <div>
         <ul>
-          {items.map((todoItem) => (
-            <li>{todoItem}</li>
+          {items.map((todoItem, index) => (
+            <ToDoItem
+              key={index}
+              id={index}
+              text={todoItem}
+              onChecked={deleteItem}
+            />
           ))}
         </ul>
       </div>
