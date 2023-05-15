1ef3f3082a7c998ac68cd79093baa4636eb96f21
ELIJDAYO <95513598+ELIJDAYO@users.noreply.github.com>
2023-05-15 08:44:47 +0800

    state without hook

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -3,15 +3,16 @@ import React from 'react';
 var isDone = false;
 
 function strike() {
-  isDone = true;
+  document.getElementById('root').style.textDecoration = 'line-through';
 }
 
 function unStrike() {
-  isDone = false;
+  document.getElementById('root').style.textDecoration = null;
 }
 
 function App() {
   return (
+    // not changeable, needs to be rerendered somewhere
     <div>
       <p style={isDone ? { textDecoration: 'line-through' } : null}>Buy milk</p>
       <button onClick={strike}>Change to strike through</button>
