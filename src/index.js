a83ea812249c528f96838d5e55088bb9a5c4f169
ELIJDAYO <95513598+ELIJDAYO@users.noreply.github.com>
2023-05-15 17:56:31 +0800

    implemented destructuring

--- src/index.js
+++ src/index.js
@@ -1,25 +1,37 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './components/App';
+import cars from './practice.js';
+
+const [honda, tesla] = cars;
+const {
+  speedStats: { topSpeed: teslaTopSpeed },
+  coloursByPopularity: [teslaTopColour, teslaSecondCol],
+} = tesla;
+const {
+  speedStats: { topSpeed: hondaTopSpeed },
+  coloursByPopularity: [hondaTopColour, hondaSecondCol],
+} = honda;
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root
-  .render
-  //   <table>
-  //     <tr>
-  //       <th>Brand</th>
-  //       <th>Top Speed</th>
-  //     </tr>
-  //     <tr>
-  //       <td>{tesla.model}</td>
-  //       <td>{teslaTopSpeed}</td>
-  //       <td>{teslaTopColour}</td>
-  //     </tr>
-  //     <tr>
-  //       <td>{honda.model}</td>
-  //       <td>{hondaTopSpeed}</td>
-  //       <td>{hondaTopColour}</td>
-  //     </tr>
-  //   </table>,
-  //   document.getElementById("root")
-  ();
+root.render(
+  <table>
+    <tr>
+      <th>Brand</th>
+      <th>Top-Speed</th>
+      <th>Top-Color1</th>
+      <th>Top-Color2</th>
+    </tr>
+    <tr>
+      <td>{tesla.model}</td>
+      <td>{teslaTopSpeed}</td>
+      <td>{teslaTopColour}</td>
+      <td>{teslaSecondCol}</td>
+    </tr>
+    <tr>
+      <td>{honda.model}</td>
+      <td>{hondaTopSpeed}</td>
+      <td>{hondaTopColour}</td>
+      <td>{hondaSecondCol}</td>
+    </tr>
+  </table>
+);
