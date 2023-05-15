4da772fd1400b6484c2d4763b204e706f8b51fbc
ELIJDAYO <95513598+ELIJDAYO@users.noreply.github.com>
2023-05-15 22:50:50 +0800

    template for changing complex state

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,31 +1,12 @@
-import React, { useState } from 'react';
+import React from 'react';
 
 function App() {
-  const [name, setName] = useState('');
-  const [headingText, setHeading] = useState('');
-
-  function handleChange(event) {
-    setName(event.target.value);
-  }
-
-  function handleClick(event) {
-    setHeading(name);
-    // form cannot referesh the page
-    event.preventDefault();
-  }
-
   return (
     <div className="container">
-      {/* can't use {name} bec of realtime update by onChange */}
-      <h1>Hello {headingText}</h1>
-      <form onSubmit={handleClick}>
-        <input
-          onChange={handleChange}
-          type="text"
-          placeholder="What's your name?"
-          // value comes from handleChange()
-          value={name}
-        />
+      <h1>Hello</h1>
+      <form>
+        <input name="fName" placeholder="First Name" />
+        <input name="lName" placeholder="Last Name" />
         <button>Submit</button>
       </form>
     </div>
