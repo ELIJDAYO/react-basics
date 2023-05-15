7283295c5ad29ff38b113168c1776e664a1e2e11
ELIJDAYO <95513598+ELIJDAYO@users.noreply.github.com>
2023-05-15 17:24:04 +0800

    template for object and array destructuring

--- src/index.js
+++ src/index.js
@@ -3,4 +3,23 @@ import ReactDOM from 'react-dom/client';
 import App from './components/App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<App />);
+root
+  .render
+  //   <table>
+  //     <tr>
+  //       <th>Brand</th>
+  //       <th>Top Speed</th>
+  //     </tr>
+  //     <tr>
+  //       <td>{tesla.model}</td>
+  //       <td>{teslaTopSpeed}</td>
+  //       <td>{teslaTopColour}</td>
+  //     </tr>
+  //     <tr>
+  //       <td>{honda.model}</td>
+  //       <td>{hondaTopSpeed}</td>
+  //       <td>{hondaTopColour}</td>
+  //     </tr>
+  //   </table>,
+  //   document.getElementById("root")
+  ();
