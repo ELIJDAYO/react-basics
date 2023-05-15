0c5de1a7df8dde4bf56d3d9ab9309bca4e017541
ELIJDAYO <95513598+ELIJDAYO@users.noreply.github.com>
2023-05-15 10:30:10 +0800

    implemented react hooks

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,7 +1,27 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 function App() {
-  return <div />;
+  // param is starting state
+  // state is an array
+  // count is val, setCount is update func
+  const [count, setCount] = useState(0);
+
+  function increase() {
+    // in order to update...need to rerender
+    setCount(count + 1);
+  }
+  function decrease() {
+    // in order to update...need to rerender
+    setCount(count - 1);
+  }
+
+  return (
+    <div className="container">
+      <h1>{count}</h1>
+      <button onClick={increase}>+</button>
+      <button onClick={decrease}>-</button>
+    </div>
+  );
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -3,11 +3,4 @@ import ReactDOM from 'react-dom/client';
 import App from './components/App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <div>
-    <div className="container">
-      <h1>0</h1>
-      <button>+</button>
-    </div>
-  </div>
-);
+root.render(<App />);
